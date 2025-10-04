5fb0f6590dc0c2b3ae59e9f347102a58fddfb150
zidanindratama <zidanindratama31@gmail.com>
2025-10-04 09:07:42 +0700

    feat: major changes add parameter to SayHello functiong

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/zidanindratama/go-say-hello
+module github.com/zidanindratama/go-say-hello/v2
 
 go 1.23.4
--- say_hello.go
+++ say_hello.go
@@ -1,5 +1,5 @@
 package gosayhello
 
-func SayHello() string {
-	return "Hello World!"
+func SayHello(name string) string {
+	return "Hello " + name
 }
